c74c97578e6e56d7f138f70ed0a0e6138862a7d4
Aiden Fisher <fisher.aiden.b@gmail.com>
2026-06-25 12:21:31 -0400

    bin.sh command for cron runs

--- cron.sh
+++ cron.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
-curl -s -X POST "${WEBHOOK_BASE}/run" \
-  -H "X-Secret: ${WEBHOOK_SECRET}" \
-  -H "Content-Type: application/json"
+python3 -c "
+import os, requests, sys
+resp = requests.post(
+    os.environ['WEBHOOK_BASE'].rstrip('/') + '/run',
+    headers={'X-Secret': os.environ['WEBHOOK_SECRET'], 'Content-Type': 'application/json'},
+    json={},
+    timeout=10,
+)
+print(resp.status_code, resp.text)
+sys.exit(0 if resp.status_code in (200, 409) else 1)
+"
